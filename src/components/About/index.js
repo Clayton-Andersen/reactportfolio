b9c2c961bd570c043f697f0005be3e9a7adc6035
Aloha <utah.clayton@gmail.com>
2022-10-06 23:32:25 -0700

    minor changes to contact section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,8 +3,10 @@ const About = () => {
         <div id="about-me" className="about">
             <div className="about-me">
                 <h3>A little about me:</h3>
-                <p>As a student of full-stack development, I'm equipped with the freshest set of skills to bring
-                    big ideas to life.</p>
+                <p>MERN Full-Stack Web Developer with strong background in management and leadership. Awarded University
+                    of Utah Coding Bootcamp Certificate following competent demonstration of skills via numerous
+                    mobile-first web applications developed both personally and as a valuable team contributor.
+                    Application of Object Oriented Programming concepts to modulate development and simplify code.</p>
             </div>
             <div>
                 <img src="./assets/images/LinkedIn.JPG" alt="Profile Photo of Clayton Andersen" />
--- src/components/Showcase/index.js
+++ src/components/Showcase/index.js
@@ -8,15 +8,15 @@ const Showcase = () => {
     <div className="flex-row">
       <h4 className="titles">Run Buddy</h4>
       <a href="https://github.com/Clayton-Andersen/run-buddy.git">
-        <img src="./assets/images/Egg.jpeg" />
+        <img src="./assets/images/Egg.jpeg" alt="" />
       </a>
       <h4 className="titles">Horiseon</h4>
       <a href="https://github.com/Clayton-Andersen/Challenge-1-Horiseon.git">
-        <img src="./assets/images/Cherry.jpeg" />
+        <img src="./assets/images/Cherry.jpeg" alt="" />
       </a>
       <h4 className="titles">Robot Gladiators</h4>
       <a href="https://github.com/Clayton-Andersen/robot-gladaitors.git">
-        <img src="./assets/images/Aspen Glow 8X10.jpg" />
+        <img src="./assets/images/Aspen Glow 8X10.jpg" alt="" />
       </a>
     </div>
   </div>
