a150c951b61430612bdf1883d2a91bc3ab7ae080
Aloha <utah.clayton@gmail.com>
2022-10-08 22:47:18 -0700

    Revert "minor changes to contact section"
    This reverts commit b9c2c961bd570c043f697f0005be3e9a7adc6035.

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,20 +1,25 @@
 const Contact = () => {
-    return (
-        <div id="contact" className="get-in-touch">
+  return (
+    <div id="contact" className="get-in-touch">
       <h3>Get In Touch:</h3>
       <div>
-        <a href="mailto:utah.clayton@gmail.com">utah.clayton@gmail.com</a>
+        <h3>
+          <a href="mailto:utah.clayton@gmail.com">utah.clayton@gmail.com</a>
+        </h3>
       </div>
       <div>
-        <h3>Socials</h3>
-        <ul>
-          <li>
-            <p>Social Media Comoing Soon!</p>
-          </li>
-        </ul>
+        <h3>
+          <a href="https://www.linkedin.com/in/clayton-andersen-103b2438/">LinkedIn</a>
+        </h3>
+      </div>
+      <div>
+        <h3>
+          <a
+            href="https://docs.google.com/document/d/1HxyOoffFE6pQ8q9wpGtrO-pgv8tfBvcKZCTKOTALd7I/edit?usp=sharing">Resume</a>
+        </h3>
       </div>
     </div>
-    )
+  )
 }
 
 export default Contact
